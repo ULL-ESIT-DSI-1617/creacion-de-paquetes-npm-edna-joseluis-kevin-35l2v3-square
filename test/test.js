d338ae078d0f8e060313d1767d2abfeebf1b7548
alu0100821390 <alu0100821390@ull.edu.es>
2017-04-26 10:42:34 +0100

    Añadir más pruebas

--- test/test.js
+++ test/test.js
@@ -1,4 +1,13 @@
-let Square = require('../lib/square');
+var Square = require('../lib/square');
+var Shape = require('@ull-edna-joseluis-kevin-35l2/ull-shape');
+
+describe("getArea Shape", function(){
+	it("must compute the square shape area correctly", function(){
+		let sh = new Shape ({ width: 100 }, 'Square');
+		let result = sh.getArea();
+		result.should.equal(10000);
+	})
+});
 
 describe ("getArea Square", function() {
 	it ("must compute the square area correctly", function() {
